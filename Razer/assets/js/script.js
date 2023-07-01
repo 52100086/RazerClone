24f820a1cb0436966e17b1b34a22eff55fa91f77
52100086 <52100086@student.tdtu.edu.vn>
2023-07-01 11:19:02 +0700

    '..'

--- Razer/assets/js/script.js
+++ Razer/assets/js/script.js
@@ -2,6 +2,16 @@ var cart_btn = $('#cart');
 var subnav   = $('#sub');
 
 
-cart_btn.on('click', function (){
-    subnav.toggle();
+cart_btn.on('click', function (e){
+    e.preventDefault();
+    subnav.css('display', 'block');
+    e.stopPropagation();
+});
+
+
+$(document).on('click', function (e) {
+    // Check if the click target is not a descendant of #sub
+    if (!subnav.is(e.target) && subnav.has(e.target).length === 0) {
+      subnav.css('display', 'none');
+    }
 });
